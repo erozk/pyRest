334de796e7f9b7af2f6e9ab40ada67e30b9b93cc
erozk <ozkayaeo@outlook.com>
2022-08-23 04:33:40 +0300

    manager.py ready for run

--- manage.py
+++ manage.py
@@ -7,15 +7,10 @@ from app.main import create_app, db
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
 app.register_blueprint(blueprint,url_prefix='/api')
-
 app.app_context().push()
-
 manager = Manager(app)
-
 migrate = Migrate(app, db)
-
 manager.add_command('db', MigrateCommand)
-
 
 @manager.command
 def run():
@@ -34,15 +29,4 @@ def test():
 if __name__ == '__main__':         
     app.run()
     # manager.run()
-
-
-
-# @app.after_request
-# def after_request(response):
-#   response.headers.add('Access-Control-Allow-Origin', '*')
-#   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-#   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-#   response.headers.add('Access-Control-Allow-Credentials', 'true')
-#   return response
-
 
